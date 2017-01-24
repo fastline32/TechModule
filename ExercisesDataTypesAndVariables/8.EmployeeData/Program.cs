bc3eb525acc591c778b0c8db40749b003338ba7d
fastline32 <speednet@mail.bg>
2017-01-24 20:22:07 +0200

    New One

--- ExercisesDataTypesAndVariables/8.EmployeeData/Program.cs
+++ ExercisesDataTypesAndVariables/8.EmployeeData/Program.cs
@@ -10,6 +10,19 @@ namespace _8.EmployeeData
     {
         static void Main(string[] args)
         {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            long personalID = long.Parse(Console.ReadLine());
+            uint employeeID = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("First name: {0}",firstName);
+            Console.WriteLine("Last name: {0}",lastName);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Gender: {0}",gender);
+            Console.WriteLine("Personal ID: {0}", personalID);
+            Console.WriteLine("Unique Employee number: {0}",employeeID);
         }
     }
 }
